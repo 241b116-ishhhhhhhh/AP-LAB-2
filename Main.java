fdad6e1500f0f400c4c431d9721c6e1d38ac47e9
241B116 <241B116@juet.co.in>
2026-08-03 12:46:28 +0530

    Lab-1

---
+++ Main.java
@@ -0,0 +1,11 @@
+
+public class Main {
+    public static void main(String[] args) {
+        Add a1 = new Add();
+        int result = a1.sum(5,6);
+        System.out.println(result);
+        }
+    }
+
+
+
